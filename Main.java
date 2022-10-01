e620701314bb5afd4ab472a10f98cb182bc96354
JuliaKoga <112908764+JuliaKoga@users.noreply.github.com>
2022-09-30 21:50:46 -0300

    arrumei o tipo da matriz desenho

--- Main.java
+++ Main.java
@@ -25,7 +25,7 @@ public class Main {
     }
 
     public static void desenho(){
-        String[][] desenho = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+        char[][] desenho = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
         for(int i = 0; i< desenho.length; i++){
             for(int j = 0; j< desenho.length; j++){
                 if(j!=2){
