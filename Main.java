7180c4592d824de6059145b69fa6715ab6eda0cb
flaviaadiniz <flaviaadiniz@gmail.com>
2022-10-01 19:57:46 -0300

    adicionei switch case para as jogadas

--- Main.java
+++ Main.java
@@ -9,14 +9,64 @@ public class Main {
 
         char[][] posicao = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
         int vez=0;
-        for(int i=0;i<=9;i++){
+        for(int i=0;i<=8;i++){
           if(vez<=0){
               escolha(jogador1);
+              Scanner sc = new Scanner(System.in);
+              char posicaoEscolhida = sc.next().charAt(0);
+
+              switch (posicaoEscolhida) {
+                  case '1': posicao[0][0] = 'X';
+                  break;
+                  case '2': posicao[0][1] = 'X';
+                  break;
+                  case '3': posicao[0][2] = 'X';
+                  break;
+                  case '4': posicao[1][0] = 'X';
+                  break;
+                  case '5': posicao[1][1] = 'X';
+                  break;
+                  case '6': posicao[1][2] = 'X';
+                  break;
+                  case '7': posicao[2][0] = 'X';
+                  break;
+                  case '8': posicao[2][1] = 'X';
+                  break;
+                  case '9': posicao[2][2] = 'X';
+                  break;
+                  default:
+                      System.out.println("Opção inválida!");
+              }
               desenho(posicao);
               vez++;
           }
           else {
               escolha(jogador2);
+              Scanner sc = new Scanner(System.in);
+              char posicaoEscolhida = sc.next().charAt(0);
+
+              switch (posicaoEscolhida) {
+                  case '1': posicao[0][0] = 'O';
+                      break;
+                  case '2': posicao[0][1] = 'O';
+                      break;
+                  case '3': posicao[0][2] = 'O';
+                      break;
+                  case '4': posicao[1][0] = 'O';
+                      break;
+                  case '5': posicao[1][1] = 'O';
+                      break;
+                  case '6': posicao[1][2] = 'O';
+                      break;
+                  case '7': posicao[2][0] = 'O';
+                      break;
+                  case '8': posicao[2][1] = 'O';
+                      break;
+                  case '9': posicao[2][2] = 'O';
+                      break;
+                  default:
+                      System.out.println("Opção inválida!");
+              }
               desenho(posicao);
               vez--;
           }
@@ -53,6 +103,19 @@ public class Main {
     }
 
     public static void escolha(String nome){
-        System.out.println(nome+" escolha uma posição:");
+
+        System.out.println(nome+" escolha uma posição de 1 a 9:");
     }
+
+
+    /*public static void vencedor(String resultado){
+        if (resultado.equals(jogador1)) {
+            System.out.println(jogador1 + "venceu!");
+        } else if (resultado.equals(jogador2)) {
+            System.out.println(jogador2 + "venceu!");
+        } else {
+            System.out.println("Deu velha!");
+        }
+    }*/
+
 }
