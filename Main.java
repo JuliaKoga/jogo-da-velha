c4e109f83ec97ec2b8d624012bd645f5651c5818
JuliaKoga <112908764+JuliaKoga@users.noreply.github.com>
2022-09-30 21:44:30 -0300

    retirei posicao do main e criei um método

--- Main.java
+++ Main.java
@@ -7,17 +7,7 @@ public class Main {
         String jogador1 = getJogador("Jogador 1");
         String jogador2 = getJogador("Jogador 2");
 
-        String[][] posicao = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
-        for(int i=0;i<posicao.length;i++){
-            for(int j=0;j< posicao.length;j++){
-                if(j!=2){
-                    System.out.print(posicao[i][j]+"|");
-                }else{
-                    System.out.println(posicao[i][j]);
-                }
-            }
-
-        }
+        desenho();
 
     }
 
@@ -31,6 +21,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o nome do " + nome);
         return sc.nextLine();
+
+    }
+
+    public static void desenho(){
+        String[][] desenho = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+        for(int i = 0; i< desenho.length; i++){
+            for(int j = 0; j< desenho.length; j++){
+                if(j!=2){
+                    System.out.print(desenho[i][j]+" | ");
+                }else{
+                    System.out.println(desenho[i][j]);
+                }
+            }
+
+        }
 
     }
 
